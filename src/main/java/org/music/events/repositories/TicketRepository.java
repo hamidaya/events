bcb5bab6e157a30264e5c755493a1778cacbee03
hamidaya <info@ayachi.com>
2024-05-18 09:58:30 +0200

    aangepast voor validate

--- src/main/java/org/music/events/repositories/TicketRepository.java
+++ src/main/java/org/music/events/repositories/TicketRepository.java
@@ -1,4 +1,5 @@
 package org.music.events.repositories;
+import org.music.events.models.QRCodeImage;
 import org.music.events.models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ import java.util.Optional;
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 //    Ticket  (String qrCode);
     Ticket findByQrCode(String qrCode);
+
+    QRCodeImage findTicketByQrCode(Long qrCodeId);
 }
