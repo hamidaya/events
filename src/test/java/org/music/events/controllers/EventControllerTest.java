a5e891fd77e3fd0c0917f91731472b9707b3d5fe
pa38ny <hamid.ayachi@ing.com>
2024-05-30 21:16:05 +0200

    EventControllerTest unittest.

--- src/test/java/org/music/events/controllers/EventControllerTest.java
+++ src/test/java/org/music/events/controllers/EventControllerTest.java
@@ -20,6 +20,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -30,47 +32,40 @@ class EventControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private EventService eventService;
+    private EventService eventServiceTest;
 
     Event event1;
     Event event2;
     Event event3;
 
-    EventRequestDTO eventRequestDTO1;
-    EventRequestDTO eventRequestDTO2;
-
-    EventRespondsDTO eventRespondsDTO1;
     EventRespondsDTO eventRespondsDTO2;
+
 
     @BeforeEach
     public void setUp() {
-        event1 = new Event(1L, "mark event", "party", "utrecht", LocalDate.of(2024, 02, 04), LocalDate.of(2024, 07, 15), 34, 456, "dit event alleen voor mark en frans");
-        event2 = new Event(2L, "frans event", "festival", "Amsterdam", LocalDate.of(2024, 02, 04), LocalDate.of(2024, 07, 15), 34, 456, "dit event alleen voor mark en frans");
-        event3 = new Event(3L, "hamid event", "festival", "nijmegen", LocalDate.of(2024, 02, 04), LocalDate.of(2024, 07, 15), 34, 456, "dit event alleen voor mark en frans");
-
+        event1 = new Event(1L, "mark event", "party", "utrecht", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34, 456, "dit event alleen voor mark en frans");
+        event2 = new Event(2L, "frans event", "festival", "Amsterdam", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34, 456, "dit event alleen voor mark en frans");
+        event3 = new Event(3L, "hamid event", "festival", "nijmegen", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34, 456, "dit event alleen voor mark en frans");
     }
-
+//
     @Test
     void getAllEvents() throws Exception {
-        given(eventService.getAllEvents()).willReturn(List.of(eventRespondsDTO1,eventRespondsDTO2));
+        given(eventServiceTest.getAllEvents()).willReturn(List.of(eventRespondsDTO2));
 
         mockMvc.perform(get("/events"))
                 .andExpect(status().isOk())
-
-
-                .andExpect(jsonPath("$[0].eventId").value("9789076174105"))
-                .andExpect(jsonPath("$[0].eventName").value("Harry Potter"))
-                .andExpect(jsonPath("$[0].subtitle").value("en de steen der wijzen"))
-                .andExpect(jsonPath("$[0].genre").value("fiction"))
-                .andExpect(jsonPath("$[0].language").value("NL"))
-                .andExpect(jsonPath("$[0].type").value("paperback"))
-                .andExpect(jsonPath("$[0].publisher").value("uitgeverij de Harmonie"))
-                .andExpect(jsonPath("$[0].authorDto.id").value("1"))
-                .andExpect(jsonPath("$[0].authorDto.initials").value("J.K."));
-
-        String isbn, String title, String subtitle, String genre, String language, String type,
-                String publisher, AuthorDto authorDto)
-
-
-        }
+                .andExpect(jsonPath("$[0].eventId").value(1))
+                .andExpect(jsonPath("$[0].eventName").value("mark event"))
+                .andExpect(jsonPath("$[0].eventType").value("party"))
+                .andExpect(jsonPath("$[0].eventLocation").value("utrecht"))
+                .andExpect(jsonPath("$[0].eventStartDate").value("2024-02-04"))
+                .andExpect(jsonPath("$[0].eventEndDate").value("2024-07-15"))
+                .andExpect(jsonPath("$[0].maxParticipants").value(34))
+                .andExpect(jsonPath("$[0].ticketPrice").value(456))
+                .andExpect(jsonPath("$[0].description").value("dit event alleen voor mark en frans"));
     }
+
+}
+
+
+
