2d05c9a56ba70ffc4cc273ffc0a9a47c23586071
pa38ny <hamid.ayachi@ing.com>
2024-06-03 21:09:23 +0200

    EventControllerTest unittesten.

--- src/test/java/org/music/events/controllers/EventControllerTest.java
+++ src/test/java/org/music/events/controllers/EventControllerTest.java
@@ -3,41 +3,44 @@ package org.music.events.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.music.events.filter.JwtRequestFilter;
 import org.music.events.services.CustomUserDetailsService;
+import org.music.events.services.EventService;
 import org.music.events.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.music.events.dtos.EventRequestDTO;
 import org.music.events.dtos.EventRespondsDTO;
 import org.music.events.models.Event;
-import org.music.events.services.EventService;
-
-
 import java.time.LocalDate;
-import java.util.List;
 
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(EventController.class)
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc(addFilters = false)
+@SpringBootTest
+
 class EventControllerTest {
+
+    @MockBean
+    private EventService eventService;
 
     @Autowired
     private MockMvc mockMvc;
-
-
-    @MockBean
-    private EventService eventServiceTest;
 
     @MockBean
     private JwtUtil jwtUtilTest;
@@ -56,30 +59,34 @@ class EventControllerTest {
 
     EventRespondsDTO eventRespondsDTO2 = new EventRespondsDTO();
 
-
-
     @BeforeEach
     public void setUp() {
+
+        eventRespondsDTO2 = new EventRespondsDTO(1L,"party", "mark event", "utrecht",  LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15),456.00,34,"dit event alleen voor mark en frans");
+
         event1 = new Event(1L, "mark event", "party", "utrecht", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34.80, 456, "dit event alleen voor mark en frans");
         event2 = new Event(2L, "frans event", "festival", "Amsterdam", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34.32, 456, "dit event alleen voor mark en frans");
         event3 = new Event(3L, "hamid event", "festival", "nijmegen", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34.35, 456, "dit event alleen voor mark en frans");
     }
 
     @Test
-    void getAllEvents() throws Exception {
-        when(eventServiceTest.getAllEvents()).thenReturn(List.of(eventRespondsDTO2));
+    @WithMockUser(username = "admin", password = "password", roles = "ADMIN")
 
-        mockMvc.perform(get("/events"))
+    void getAllEvents() throws Exception {
+//        List<EventRespondsDTO> = List.of(eventRespondsDTO2);
+//        when(eventService.getAllEvents()).thenReturn(eventRespondsDTOArrayList);
+
+        mockMvc.perform(get("/events").with(httpBasic("admin", "password")))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].eventId").value(1))
+                .andExpect(jsonPath("$[0].eventId").value(1L))
                 .andExpect(jsonPath("$[0].eventName").value("mark event"))
                 .andExpect(jsonPath("$[0].eventType").value("party"))
                 .andExpect(jsonPath("$[0].eventLocation").value("utrecht"))
                 .andExpect(jsonPath("$[0].eventStartDate").value("2024-02-04"))
                 .andExpect(jsonPath("$[0].eventEndDate").value("2024-07-15"))
-                .andExpect(jsonPath("$[0].maxParticipants").value(34))
-                .andExpect(jsonPath("$[0].ticketPrice").value(456))
-                .andExpect(jsonPath("$[0].description").value("dit event alleen voor mark en frans"));
+                .andExpect(jsonPath("$[0].availableTickets").value(34))
+                .andExpect(jsonPath("$[0].eventPrice").value(456.00))
+                .andExpect(jsonPath("$[0].eventDescription").value("dit event alleen voor mark en frans"));
 
     }
 
--- src/test/java/org/music/events/services/EventServiceTest.java
+++ src/test/java/org/music/events/services/EventServiceTest.java
@@ -1,6 +1,5 @@
 package org.music.events.services;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +15,7 @@ import org.music.events.dtos.EventRespondsDTO;
 import org.music.events.models.Event;
 import org.music.events.repositories.EventRepository;
 import org.music.events.services.EventService;
+import org.springframework.context.annotation.Profile;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -24,7 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
-
 class EventServiceTest {
     @Mock
     EventRepository eventRepository;
@@ -35,14 +34,12 @@ class EventServiceTest {
     Event event1;
     Event event2;
     Event event3;
-
     @BeforeEach
     void setUp() {
         event1 = new Event(1L, "mark event", "party", "utrecht", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34.80, 456, "dit event alleen voor mark en frans");
         event2 = new Event(2L, "frans event", "festival", "Amsterdam", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34.32, 456, "dit event alleen voor mark en frans");
         event3 = new Event(3L, "hamid event", "festival", "nijmegen", LocalDate.of(2024, 2, 4), LocalDate.of(2024, 7, 15), 34.35, 456, "dit event alleen voor mark en frans");
     }
-
     @Test
     void getAllEvents() {
         when(eventRepository.findAll()).thenReturn(List.of(event1, event2, event3));
